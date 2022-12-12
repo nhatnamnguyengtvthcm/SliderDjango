ee00b030f13411cd9d48e6d277ca04359173053d
Namnn <nhatnam.nguyen@masterisehomes.com>
2022-12-12 14:31:28 +0700

    up

--- django_sliders/build/lib/slider/urls.py
+++ django_sliders/build/lib/slider/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'slider'
 
 urlpatterns = [
-
+    path("", views.SliderIndex.as_view(), name="sliderIndex")
 ]
--- django_sliders/slider/views.py
+++ django_sliders/slider/views.py
@@ -1,14 +1,22 @@
+import json
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 from django_sliders.slider.models import Slider
+from django_sliders.slider.serializers import SliderTemplate
 
 
 class SliderIndex(TemplateView):
     template_name = "index.html"
 
     def get(self, request):
+
         sliders = Slider.objects.all()
-        context = { "sliders": sliders }
+
+        serializers = SliderTemplate(sliders, many = True)
+
+        slider_data = json.loads(json.dumps(serializers.data))
+        context = { "sliders": sliders, "slider_data": slider_data}
 
         return render(request, self.template_name, context)
